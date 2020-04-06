2f407c73e256c336eba7b24b88dd9142194027cb
ianertson <sebbekarlsson97@gmail.com>
2020-04-06 11:23:58 +0200

    implements interactive shell, closes #108

--- src/string_utils.c
+++ src/string_utils.c
@@ -1,5 +1,6 @@
 #include "include/string_utils.h"
 #include <string.h>
+#include <stdio.h>
 
 
 char* hermes_char_to_string(char c)
@@ -15,5 +16,47 @@ char* hermes_init_str(const char* value)
 {
     char* str = calloc(strlen(value) + 1, sizeof(char));
     strcpy(str, value);
+    return str;
+}
+
+char* hermes_get_stdin(const char* printstr)
+{
+    char* str;
+    int c;
+    int i;
+    int size = 10;
+
+    str = malloc(size * sizeof(char));
+
+    printf(">: ");
+
+    for(i = 0; (c = getchar()) != '\n' && c != EOF; ++i)
+    {
+        if(i == size)
+        {
+            size = 2*size;
+            str = realloc(str, size*sizeof(char));
+
+            if(str == NULL)
+            {
+                printf("Cannot reallic string.\n");
+                exit(-1);
+            }
+        }
+
+        str[i] = c;
+    }
+
+    if(i == size)
+    {
+        str = realloc(str, (size+1)*sizeof(char));
+
+        if(str == NULL)
+        {
+            printf("Cannot realloc string.\n");
+            exit(-1);
+        }
+    }
+
     return str;
 }
